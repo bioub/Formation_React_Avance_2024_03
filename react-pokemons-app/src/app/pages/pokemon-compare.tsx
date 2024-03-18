fc6d918ac1cab312930441c49390bfe604295fed
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-03-18 14:28:33 +0100

    Correction Ex 1

---
+++ react-pokemons-app/src/app/pages/pokemon-compare.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import PokemonCardDetails from '../components/pokemon-card-details';
+import { getPokemon } from '../services/pokemon-service';
+import { Pokemon } from '../models/pokemon';
+
+type Props = Readonly<{}>;
+
+function PokemonCompare({}: Props) {
+  const [pokemon1, setPokemon1] = useState<Pokemon | undefined>(undefined);
+  const [pokemon2, setPokemon2] = useState<Pokemon | undefined>(undefined);
+
+  useEffect(() => {
+    getPokemon(1).then((pokemon) => setPokemon1(pokemon));
+    getPokemon(2).then((pokemon) => setPokemon2(pokemon));
+  }, []);
+
+  return (
+    <div className="PokemonCompare">
+      <div className="row">
+        <div className="col s6">
+          <PokemonCardDetails pokemon={pokemon1} />
+        </div>
+        <div className="col s6">
+          <PokemonCardDetails pokemon={pokemon2} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonCompare;
